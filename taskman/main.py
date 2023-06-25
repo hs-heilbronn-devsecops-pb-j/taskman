7ef1c6867b4168f3234ebe90fafb244eac2b5857
SturmCamper <66077206+SturmCamper@users.noreply.github.com>
2023-06-25 13:32:58 +0200

    Update main.py
    batch dose not work with cloud run

--- taskman/main.py
+++ taskman/main.py
@@ -14,7 +14,7 @@ from opentelemetry import trace
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
 app = FastAPI()
 
@@ -36,7 +36,7 @@ tracer_provider.add_span_processor(
     # BatchSpanProcessor buffers spans and sends them in batches in a
     # background thread. The default parameters are sensible, but can be
     # tweaked to optimize your performance
-    BatchSpanProcessor(cloud_trace_exporter)
+    SimpleSpanProcessor(cloud_trace_exporter)
 )
 
 trace.set_tracer_provider(tracer_provider)
